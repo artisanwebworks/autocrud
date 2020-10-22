0802d4aa2e4c5b05df17b8534f4817dae09d791f
Dave Dosanjh <6342618+ddosanjh@users.noreply.github.com>
2020-10-22 09:43:44 -0700

    bulk create for sub resource

--- src/GenericAPIController.php
+++ src/GenericAPIController.php
@@ -402,12 +402,10 @@ class GenericAPIController extends BaseController {
         // If a sub-resource route, automatically include in creation arguments
         // the foreign key field referencing the parent.
         $argsSet = $req->all();
-        Log::info("inserting parent id (fk name)", [$schema->parentForeignKeyName]);
         if ($schema->parent) {
-          foreach ($argsSet as $args) {
-            $args[$schema->parentForeignKeyName] = end($uriIdStack);
+          foreach ($argsSet as $i => $args) {
+            $argsSet[$i][$schema->parentForeignKeyName] = end($uriIdStack);
           }
-          Log::info("added fk to args set", $argsSet);
         }
 
         // Instantiate (but don't yet save) the new model.
